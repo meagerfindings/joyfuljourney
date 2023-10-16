8320c07c3c136585be39eca416efbd3abafb6d18
Mat Greten <mgreten@gmail.com>
2023-10-16 12:02:48 -0600

    feature(Post Show): add link to post index

--- spec/features/posts/show_spec.rb
+++ spec/features/posts/show_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Posts Show Page", type: :feature do
-  it "shows a specific post's details" do
-    user = User.create(first_name: "Elon", last_name: "Musk", birthdate: "June 28, 1971")
-    post = user.post.create(title: "Punny", body: "For the chemistry nerds: “Technically, alcohol is a solution.")
+  let(:user) { User.create(first_name: "Elon", last_name: "Musk", birthdate: "June 28, 1971") }
+  let(:post) { user.post.create(title: "Punny", body: "For the chemistry nerds: “Technically, alcohol is a solution.") }
 
+  it "shows a specific post's details" do
     visit "posts/#{post.id}"
 
     within("h1") do
@@ -14,5 +14,12 @@ RSpec.describe "Posts Show Page", type: :feature do
     expect(page).to have_content(post.body)
     expect(page).to have_link("Edit", href: edit_post_path(post))
     expect(page).to have_link("Destroy", href: post_path(post))
+  end
+
+  it "has a link back to posts index page" do
+    visit "posts/#{post.id}"
+
+    click_link 'Back to Posts'
+    expect(page).to have_current_path('/posts')
   end
 end
