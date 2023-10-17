2b523b915e1400846ae76d76d3e4760ab0e595dc
Mat Greten <mgreten@gmail.com>
2023-10-17 16:01:58 -0600

    spec(UsersController): add GET #show, GET #new, DELETE #destroy

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,29 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { User.create(first_name: "Amos", last_name: "Burton", birthdate: "July 4, 3315") }
+
   describe 'GET #index' do
     it 'renders index' do
       get :index
       expect(response).to be_successful
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #show" do
+    it "renders show" do
+      get :show, params: { id: user.to_param }
+      expect(response).to be_successful
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "GET #new" do
+    it "renders new" do
+      get :new
+      expect(response).to be_successful
+      expect(response).to render_template(:new)
     end
   end
 
@@ -35,7 +53,6 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #edit" do
     it "renders edit" do
-      user = User.create(first_name: "Amos", last_name: "Burton", birthdate: "July 4, 3315")
       get :edit, params: { id: user.to_param }
       expect(response).to be_successful
       expect(response).to render_template(:edit)
@@ -43,8 +60,6 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "PUT #update" do
-    let(:user) { User.create(first_name: "Amos", last_name: "Burton", birthdate: "July 4, 3315") }
-
     context "with valid parameters" do
       let(:new_attributes) { { first_name: "Tim" } }
 
@@ -66,6 +81,27 @@ RSpec.describe UsersController, type: :controller do
         expect(response).to have_http_status(422)
         expect(response).to render_template(:edit)
       end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before do
+      user # forces the initial user to be loaded
+      User.create(first_name: "First", last_name: "Last")
+    end
+
+    it "destroys the requested user" do
+      prior_user_count = User.all.count
+
+      delete :destroy, params: { id: user.to_param }
+
+      after_user_count = User.all.count
+      expect(after_user_count).to eq(prior_user_count - 1)
+    end
+
+    it "redirects to the users list" do
+      delete :destroy, params: { id: user.to_param }
+      expect(response).to redirect_to(users_path)
     end
   end
 end
