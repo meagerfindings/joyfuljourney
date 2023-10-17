53816ce142e07eed945779dd6fa02230cb1526f1
Mat Greten <mgreten@gmail.com>
2023-10-17 08:57:24 -0600

    feature(User): display created_at on index

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users Index Page", type: :feature do
-  it "can see all users, their name, and date of birth" do
-    user_1 = User.create(first_name: "Bart", last_name: "Simpson", birthdate: "April 1, 1990")
-    user_2 = User.create(first_name: "Sideshow", last_name: "Bob", birthdate: "January 1, 1960")
+  let(:user_1) { User.create(first_name: "Bart", last_name: "Simpson", birthdate: "April 1, 1990") }
+  let(:user_2) { User.create(first_name: "Sideshow", last_name: "Bob", birthdate: "January 1, 1960") }
 
+  before do
+    user_1.touch
+    user_2.touch
+  end
+
+  it "can see all users and their information" do
     visit "/users"
 
     expect(page).to have_content("All Users")
@@ -13,6 +18,7 @@ RSpec.describe "Users Index Page", type: :feature do
       expect(page).to have_content(user_1.first_name)
       expect(page).to have_content(user_1.last_name)
       expect(page).to have_content(user_1.birthdate)
+      expect(page).to have_content(user_1.created_at)
       expect(page).to have_link(user_1.name, href: user_path(user_1.id))
       expect(page).to have_link('Edit', href: edit_user_path(user_1.id))
     end
@@ -21,6 +27,7 @@ RSpec.describe "Users Index Page", type: :feature do
       expect(page).to have_content(user_2.first_name)
       expect(page).to have_content(user_2.last_name)
       expect(page).to have_content(user_2.birthdate)
+      expect(page).to have_content(user_2.created_at)
       expect(page).to have_link(user_2.name, href: user_path(user_2.id))
       expect(page).to have_link('Edit', href: edit_user_path(user_2.id))
     end
