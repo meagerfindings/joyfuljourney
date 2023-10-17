dd6266d8e2bde5793ec2664af1509052b7e7a45e
Mat Greten <mgreten@gmail.com>
2023-10-17 15:47:25 -0600

    spec(UsersController): add GET #edit and PUT #update

--- spec/controllers/users_contorller_spec.rb
+++ spec/controllers/users_contorller_spec.rb
@@ -32,4 +32,40 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
+
+  describe "GET #edit" do
+    it "renders edit" do
+      user = User.create(first_name: "Amos", last_name: "Burton", birthdate: "July 4, 3315")
+      get :edit, params: { id: user.to_param }
+      expect(response).to be_successful
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe "PUT #update" do
+    let(:user) { User.create(first_name: "Amos", last_name: "Burton", birthdate: "July 4, 3315") }
+
+    context "with valid parameters" do
+      let(:new_attributes) { { first_name: "Tim" } }
+
+      it "updates the requested user" do
+        put :update, params: { id: user.to_param, user: new_attributes }
+        user.reload
+        expect(user.first_name).to eq(new_attributes[:first_name])
+      end
+
+      it "redirects to the user" do
+        put :update, params: { id: user.to_param, user: new_attributes }
+        expect(response).to redirect_to(user)
+      end
+    end
+
+    context "with invalid parameters" do
+      it 'does not update the user' do
+        put :update, params: { id: user.to_param, user: { first_name: '' } }
+        expect(response).to have_http_status(422)
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
 end
