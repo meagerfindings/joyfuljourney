2a04809cadb7eac061dcdc9a84b8876bbfc0f336
Mat Greten <mgreten@gmail.com>
2023-10-17 12:46:57 -0600

    spec(User Show): move user deceleration to DRY up specs

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -5,38 +5,36 @@ RSpec.describe "User Show Page", type: :feature do
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  it "displays given users complete information" do
-    user_1 = User.create(first_name: "William", middle_name: "Unknown", last_name: "Ramos", birthdate: "January 1, 1993", nickname: "Sunshine")
+  let(:user) { User.create(first_name: "William", middle_name: "Unknown", last_name: "Ramos", birthdate: "January 1, 1993", nickname: "Sunshine") }
 
-    visit "/users/#{user_1.id}"
+  it "displays given users complete information" do
+    visit "/users/#{user.id}"
 
     within("#header-name") do
-      expect(page).to have_content(user_1.name)
+      expect(page).to have_content(user.name)
     end
 
     within("#user-details") do
-      expect(page).to have_content(user_1.first_name)
-      expect(page).to have_content(user_1.middle_name)
-      expect(page).to have_content(user_1.last_name)
-      expect(page).to have_content(user_1.birthdate)
-      expect(page).to have_content(user_1.nickname)
+      expect(page).to have_content(user.first_name)
+      expect(page).to have_content(user.middle_name)
+      expect(page).to have_content(user.last_name)
+      expect(page).to have_content(user.birthdate)
+      expect(page).to have_content(user.nickname)
     end
   end
 
   it "has a link back to users index" do
-    user_1 = User.create(first_name: "William", middle_name: "Unknown", last_name: "Ramos", birthdate: "January 1, 1993", nickname: "Sunshine")
+    visit "/users/#{user.id}"
 
-    visit "/users/#{user_1.id}"
     expect(page).to have_link("All Users")
     click_link 'All Users'
     expect(page).to have_current_path('/users')
     end
 
   it "has a link back to edit user page" do
-    user_1 = User.create(first_name: "William", middle_name: "Unknown", last_name: "Ramos", birthdate: "January 1, 1993", nickname: "Sunshine")
+    visit "/users/#{user.id}"
 
-    visit "/users/#{user_1.id}"
     click_link 'Edit User'
-    expect(page).to have_current_path("/users/#{user_1.id}/edit")
+    expect(page).to have_current_path("/users/#{user.id}/edit")
   end
 end
