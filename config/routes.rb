5103ca2b88ed42a0b8b8d1db1968e1181fa9f4d7
Mat Greten <mgreten@gmail.com>
2023-10-21 05:46:42 -0600

    feature: add welcome index - changes root to be welcome

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "welcome#index"
 
   resources :posts
   resources :users do
