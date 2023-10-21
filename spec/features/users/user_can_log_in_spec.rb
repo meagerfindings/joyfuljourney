768c04a7eb056abdcac7a7489fe19e959fa56acc
Mat Greten <mgreten@gmail.com>
2023-10-21 14:45:15 -0600

    specs: log in, sign up, log out links

--- spec/features/users/user_can_log_in_spec.rb
+++ spec/features/users/user_can_log_in_spec.rb
@@ -36,4 +36,48 @@ RSpec.describe "Logging In" do
 
     expect(page).to have_content('Sorry, your username or password did not match what we have on record.')
   end
+
+  context 'when signed in' do
+    it "shows logout" do
+      visit login_path
+
+      expect(current_path).to eq(login_path)
+
+      fill_in :username, with: user.username
+      fill_in :password, with: user.password
+
+      click_on "Log In"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Log Out")
+    end
+
+    it "does not show sign up / login" do
+      visit login_path
+
+      expect(current_path).to eq(login_path)
+
+      fill_in :username, with: user.username
+      fill_in :password, with: user.password
+
+      click_on "Log In"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_content("Sign Up")
+      expect(page).to_not have_content("Log In")
+    end
+  end
+
+  context 'when signed out' do
+    it "does not show logout" do
+      visit root_path
+      expect(page).to_not have_content("Log Out")
+    end
+
+    it "does not show sign up / login" do
+      visit root_path
+      expect(page).to have_content("Sign Up")
+      expect(page).to have_content("Log In")
+    end
+  end
 end
