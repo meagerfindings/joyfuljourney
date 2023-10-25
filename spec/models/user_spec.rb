244a93bacbf7e4bd111aab4703c8c259ecda5c14
Mat Greten <mgreten@gmail.com>
2023-10-25 05:22:39 -0600

    spec(user): initial roles tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,4 +74,29 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe "roles" do
+    it "can be created as an admin" do
+      user = create(:claimed_user, role: 2)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it "can be created as a manager" do
+      user = create(:claimed_user, role: 1)
+
+      expect(user.role).to eq("manager")
+      expect(user.manager?).to be_truthy
+    end
+
+
+    it "can be created as a default user" do
+      user = create(:claimed_user, role: 0)
+
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+  end
 end
