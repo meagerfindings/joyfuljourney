b85087c5b2976348902247490fec8e22bb4f166b
Mat Greten <mat.greten@companycam.com>
2025-06-14 13:29:05 -0600

    fix(posts): update PostsController to use correct association
    Update PostsController index action to use @user.posts instead of @user.post
    to match the corrected User model association.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index
     if params[:user_id]
       @user = User.find(params[:user_id])
-      @posts = @user.post
+      @posts = @user.posts
     else
       @posts = Post.all
     end
